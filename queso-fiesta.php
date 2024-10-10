76b8c3cf294b4429e561e590d6b01ab3ad9b9644
Cristian <swear0006@gmail.com>
2024-10-10 18:54:24 -0500

    Se integran nuevas paginas de detalles
    Se integran, paginas de detalles de quesos:
    queso-cuajada.php
    queso-picado-salado.php
    queso-tipo-cheddar.php
    queso-finas-hierbas.php
    queso-fiesta.php

---
+++ queso-fiesta.php
@@ -0,0 +1,197 @@
+<!doctype html>
+<html class="no-js" lang="zxx"> 
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Queso Fiesta || Industria de Lácteos, La Maporita</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="img/icon.png">
+
+
+    <!-- CSS
+	============================================ -->
+    <!-- Plugins -->
+    <!-- <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/revoulation.css">
+    <link rel="stylesheet" href="css/plugins.css"> -->
+
+    <!-- Style Css -->
+    <!-- <link rel="stylesheet" href="style.css"> -->
+
+    <!-- Custom Styles -->
+    <!-- <link rel="stylesheet" href="css/custom.css"> -->
+    <!-- Use the minified version files listed below for better performance and remove the files listed above -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/revoulation.css">
+    <link rel="stylesheet" href="css/plugins.css">
+    <link rel="stylesheet" href="scss/style.css">
+</head>
+<body class="template-color-39 template-font-1 bg_color--3">
+    <!--[if lte IE 9]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+
+    <!-- Add your site or application content here -->
+    <!-- Wrapper -->
+    <div id="wrapper" class="wrapper">
+        <?php
+        include_once 'header.php';
+        ?>
+        <!-- Start Breadcaump Area --> 
+        <div class="brook-breadcaump-area pt--300 pt_md--250 pt_sm--200 pb--300 pb_md--250 pb_sm--200 bg_image--133 breadcaump-title-bar breadcaump-title-white" data-black-overlay='5'>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="breadcaump-inner text-center">
+                            <h2 class="heading heading-h2 font-60 text-white">Queso Fiesta</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Breadcaump Area -->
+        <!-- Page Conttent -->
+        <main class="page-content">
+            <!-- Start Single Product -->
+            <div class="brook-single-product mt--100 mt_md--80 mt_sm--60 plr--160 plr_lg--100 plr_md--50 plr_sm--50 bg_color--3">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="nextprev-btn">
+                            <div class="navlist">
+                                <div class="navitem prev">
+                                    <a id="backLink" class="text-white" href="queso-finas-hierbas.php">Anterior</a>
+                                </div>
+                                <!--
+                                <div class="navitem next">
+                                    <a id="nextLink" class="text-white" href="queso-fiesta.php">Siguiente</a>
+                                </div>-->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-12">
+                        <div class="single-product-left-side">
+                            <div class="product__details__container">
+                                <div class="tab_container big_img_container">
+                                    <div class="big_img tab-pane fade show active" id="img1" role="tabpanel">
+                                        <img class="w-100" src="img/productos/grandes/760-x-760-queso-fiesta.jpg" alt="Queso Fiesta">
+                                    </div>
+                                </div>
+                                <div class="sm_roduct_nav nav nav-tabs" role="tablist">
+                                    <a class="active" id="img1-tab" data-bs-toggle="tab" href="#img1" role="tab"
+                                        aria-controls="img1" aria-selected="true">
+                                        <img src="img/productos/mini/210-x-210-queso-fiesta.jpg" alt="Queso Fiesta">
+                                    </a>
+                                    <!--
+                                    <a id="nav-img2-tab" data-bs-toggle="tab" href="#img2" role="tab" aria-controls="img2"
+                                        aria-selected="false">
+                                        <img src="img/restaurant-two/product/small2.jpg" alt="gomes restaurant">
+                                    </a>
+                                    -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-12 mt_sm--40">
+                        <div class="single-product-details-side">
+                            <div class="product-details">
+                                <div class="inner">
+                                    <div class="header">
+                                        <h4 class="heading heading-h4 text-white">Queso Fiesta</h4>
+                                    </div>
+                                    <!--
+                                    <div class="price">
+                                        <p class="theme-color text-white">$112.00</p>
+                                    </div>
+                                    -->
+                                    <div class="description">
+                                        <p class="bk_pra text-white">Descripción Queso Fiesta
+                                            <br><br><strong>Presentaciones:</strong> información</p>
+                                    </div>
+                                    <div class="product-meta">
+                                        <div class="meta-item">
+                                            <h6 class="heading heading-h6 text-white">Categoría:</h6>
+                                            <div class="item-list">
+                                                <a class="text-white" href="#">Quesos</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bkseparator--100"></div>
+            <!-- End Single Product -->
+            <!-- Start Call To Action -->
+            <div class="brook-call-to-action bg_color--43 ptb--70">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-6 col-sm-12 col-12">
+                            <div class="call-content text-center text-md-start text-center">
+                                <h4 class="heading heading-h4 text-black">Quiero mantenerme actualizado de novedades</h4>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-sm-12 col-12">
+                            <div class="text-center text-md-end text-center mt_sm--30 mt_md--40 contact-form contact-form--4 yellow-color-2">
+                                <form action="#">
+                                    <div class="input-box">
+                                        <input type="email" placeholder="Su Correo Electrónico">
+                                        <button>Subscríbete</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Call To Action -->
+        </main>
+        <!--// Page Conttent -->
+
+        <?php
+            include_once 'footer.php';
+        ?>
+
+    </div>
+    <!--// Wrapper -->
+    <!-- Js Files -->
+    <!-- <script src="js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="js/vendor/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script> -->
+
+    <!-- REVOLUTION JS FILES -->
+    <!-- <script src="js/jquery.themepunch.tools.min.js"></script>
+    <script src="js/jquery.themepunch.revolution.min.js"></script> -->
+
+    <!-- SLIDER REVOLUTION 5.0 EXTENSIONS  (Load Extensions only on Local File Systems !  The following part can be removed on Server for On Demand Loading) -->
+    <!-- <script src="js/revolution.extension.actions.min.js"></script>
+    <script src="js/revolution.extension.carousel.min.js"></script>
+    <script src="js/revolution.extension.kenburn.min.js"></script>
+    <script src="js/revolution.extension.layeranimation.min.js"></script>
+    <script src="js/revolution.extension.migration.min.js"></script>
+    <script src="js/revolution.extension.navigation.min.js"></script>
+    <script src="js/revolution.extension.parallax.min.js"></script>
+    <script src="js/revolution.extension.slideanims.min.js"></script>
+    <script src="js/revolution.extension.video.min.js"></script> -->
+
+    <!-- Use the minified version files listed below for better performance and remove the files listed above -->
+    <script src="js/vendor/vendor.min.js"></script>
+    <script src="js/plugins.min.js"></script>
+    <!-- REVOLUTION JS FILES -->
+    <script src="js/revolution.tools.min.js"></script>
+    <!-- SLIDER REVOLUTION 5.0 EXTENSIONS -->
+    <script src="js/revolution.extension.min.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/revoulation.js"></script>
+</body>
+
+</html>
