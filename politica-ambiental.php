15feb34aae071408bd7e141746db6b9b6bb5344c
Cristian <swear0006@gmail.com>
2024-10-17 15:32:38 -0500

    Se arregla textos politica ambiental

--- politica-ambiental.php
+++ politica-ambiental.php
@@ -75,42 +75,49 @@ Como reconocimiento a la calidad de productos comprometidos con la conservación
             <div class="brook-whatdo-area ptb--150 ptb-md--80 ptb-sm--60 bg_color--3">
                 <div class="container">
                     <div class="row align-items-center">
-                        <div class="col-lg-6">
-                            <div class="thumb">
-                                <img class="w-100" src="img/about/about-us-03-image-02.jpg" alt="about images">
-                            </div>
-                        </div>
-                        <div class="col-lg-6 mt_sm--40 mt_md--40 texto-blanco">
+                        
+                        <div class="col-lg-12 mt_sm--40 mt_md--40 texto-blanco">
                             <div class="bk-title--default text-start">
-                                <h5 class="heading heading-h5 theme-color wow move-up">What we do</h5>
-                                <div class="bkseparator--30"></div>
-                                <h3 class="heading heading-h3 wow move-up">Fresh ideas & unique designs</h3>
+                                <!--<div class="bkseparator--30"></div>-->
+                                <p class="bk_pra wow move-up">Tras este compromiso de mantener el equilibrio en el ecosistema y brindar a sus territorios oportunidades de desarrollo agroindustrial, orientamos la producción de nuestros proveedores de manera responsable, a través de:</p>
                             </div>
                             <div class="row">
-
                                 <!-- Start Single -->
                                 <div class="col-lg-6 col-md-6 col-12">
                                     <div class="what-do mt--30">
                                         <div class="content">
-                                            <h5 class="heading heading-h5 wow move-up">Original ideas</h5>
                                             <div class="bkseparator--20"></div>
-                                            <p class="bk_pra wow move-up">We work with clients from all over the world.
-                                                We had worked with and serving over 2000 customers and 1000 global
-                                                companies across 13 countries in the world.</p>
+                                            <p class="bk_pra wow move-up">✓  Implementar un sistema de Gestión ambiental SGA que permita establecer una estructura ambiental encaminada a controlar y mitigar prácticas que puedan producir efectos negativos en el medio ambiente.</p>
                                         </div>
                                     </div>
                                 </div>
                                 <!-- End Single -->
-
                                 <!-- Start Single -->
                                 <div class="col-lg-6 col-md-6 col-12">
                                     <div class="what-do mt--30">
                                         <div class="content">
-                                            <h5 class="heading heading-h5 wow move-up">Graphic designs</h5>
                                             <div class="bkseparator--20"></div>
-                                            <p class="bk_pra wow move-up">Our quality of service assessment involves
-                                                controlling and managing resources by setting priorities for specific
-                                                types of clients and projects on the system.</p>
+                                            <p class="bk_pra wow move-up">✓ Una adecuada clasificación de los residuos, sensibilizando a todos los actores sobre la importancia de hacerlo.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- End Single -->
+                                 <!-- Start Single -->
+                                <div class="col-lg-6 col-md-6 col-12">
+                                    <div class="what-do mt--30">
+                                        <div class="content">
+                                            <div class="bkseparator--20"></div>
+                                            <p class="bk_pra wow move-up">✓ Dar cumplimiento a las responsabilidades ambientales, enmarcadas en la legislación ambiental vigente en el país.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- End Single -->
+                                  <!-- Start Single -->
+                                <div class="col-lg-6 col-md-6 col-12">
+                                    <div class="what-do mt--30">
+                                        <div class="content">
+                                            <div class="bkseparator--20"></div>
+                                            <p class="bk_pra wow move-up">✓ Sensibilizar a nuestro personal, proveedores, contratistas y clientes, sobre el compromiso con el medio ambiente, por medio de estrategias de concientización en el consumo de los recursos ambientales, al optimizar las acciones, generando mejoras y modernización en los equipos por modelos más eficientes.</p>
                                         </div>
                                     </div>
                                 </div>
