738826103bff25858222e99446236806efeba37d
Cristian <swear0006@gmail.com>
2024-11-12 00:05:44 -0500

    reemplazo img productos

---
+++ 2do-ciclo-de-vacunacion-contra-la-fiebre-aftosa-en-colombia.php
@@ -0,0 +1,272 @@
+<!doctype html>
+<html class="no-js" lang="zxx"> 
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>2do ciclo de vacunación contra la fiebre aftosa en Colombia | La Maporita</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="img/favicon/android-icon-192x192.png">
+
+
+    <!-- CSS
+	============================================ -->
+    <!-- Plugins -->
+    <!-- <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/revoulation.css">
+    <link rel="stylesheet" href="css/plugins.css"> -->
+
+    <!-- Style Css -->
+    <!-- <link rel="stylesheet" href="style.css"> -->
+
+    <!-- Custom Styles -->
+    <!-- <link rel="stylesheet" href="css/custom.css"> -->
+
+
+    <!-- Use the minified version files listed below for better performance and remove the files listed above -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/revoulation.css">
+    <link rel="stylesheet" href="css/plugins.css">
+    <link rel="stylesheet" href="scss/style.css">
+</head>
+
+<body class="template-color-39 template-font-1 bg_color--42">
+    <!--[if lte IE 9]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+
+    <!-- Add your site or application content here -->
+    <!-- Wrapper -->
+    <div id="wrapper" class="wrapper">
+        <?php
+            include_once 'header.php';
+        ?>
+        <!-- Start Breadcaump Area --> 
+        <div class="brook-breadcaump-area pt--300 pt_md--250 pt_sm--200 pb--300 pb_md--250 pb_sm--200 bg_image--133 breadcaump-title-bar breadcaump-title-white" data-black-overlay='5'>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="breadcaump-inner text-center">
+                            <h2 class="heading heading-h2 font-60 text-white">2do ciclo de vacunación contra la fiebre aftosa en Colombia</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Breadcaump Area -->
+
+        <!-- Page Conttent -->
+        <main class="page-content">
+            <!-- Start Blog Details -->
+            <div class="brook-blog-details-area pt--150 pb--150">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-8 col-md-12 col-sm-12 col-12">
+                            <div class="blog-details-wrapper">
+                                <article class="blog-post standard-post">
+                                    <!-- Start Header -->
+                                    <div class="header mb--40 text-center">
+                                        <!--<h3 class="heading heading-h3 font-32 text-white">Why DIY Tools were Rejected by the
+                                            Market?</h3>-->
+                                        <div class="post-meta mt--20">
+                                            <div class="post-date text-white">Ago 28, 2024</div>
+                                            <div class="post-category"><a class="text-white" href="#">Categoría</a></div>
+                                        </div>
+                                    </div>
+                                    <!-- Start Thumbnail -->
+                                    <div class="thumbnail mb--60">
+                                        <img class="w-100" src="img/novedades/detalle-novedad.jpg" alt="La Maporita">
+                                    </div>
+                                    <!-- Start Content -->
+                                    <div class="content basic-dark2-line-1px pb--50 mb--35">
+                                        <div class="inner">
+                                            <h5 class="heading heading-h5 line-height-1-95 text-white wow move-up">El 28 de octubre inicia en Colombia el segundo ciclo de vacunación contra fiebre aftosa, brucelosis bovina y rabia silvestre, liderado por el ICA y Fedegan para fortalecer la sanidad ganadera.</h5>
+                                            <div class="desc mt--45 mb--50">
+                                                <p class="bk_pra text-white wow move-up">
+                                                El 28 de octubre inicia el segundo ciclo de vacunación de 2024 en Colombia contra la fiebre aftosa, brucelosis bovina y rabia de origen silvestre. El objetivo es vacunar a aproximadamente 29,7 millones de bovinos y bufalinos en más de 616,000 predios en todo el país. Este esfuerzo, coordinado por el ICA y Fedegan, involucra a más de 6,000 personas, incluidas 83 organizaciones ejecutoras, y durará 50 días.<br><br>
+                                                La vacunación es obligatoria en zonas de riesgo para fiebre aftosa y rabia, mientras que en áreas con estatus de "libres de fiebre aftosa sin vacunación", como San Andrés y algunas regiones de la Amazonía, no será necesaria. El proceso incluye un seguimiento exhaustivo por parte del ICA y el registro digital de cada vacunación para mejorar la inmunidad de rebaño y asegurar la salud y competitividad de la ganadería colombiana.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- Start Footer -->
+                                    <div class="blog-footer mb--85 wow move-up">
+                                        <div class="row">
+                                            <div class="col-lg-6 col-sm-6 col-12">
+                                                <div class="post-tag d-flex align-items-center">
+                                                    <h6 class="heading heading-h6 text-white">Etiquetas:</h6>
+                                                    <div class="blog-tag-list pl--5">
+                                                        <a class="text-white" href="#">quesos</a>
+                                                        <a class="text-white" href="#">lacteos</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                           
+                                        </div>
+                                    </div>
+                                </article>
+                                <!-- Start Post Nav Links -->
+                                <div class="post-nav-lisnt mb--45 wow move-up">
+                                    <div class="nav-item previous">
+                                        <a href="#">
+                                            <div class="link-text">
+                                                <span class="fa fa-arrow-left"></span>
+                                                <p class="">Previa</p>
+                                            </div>
+                                            <span>Laboratorio Digital para Emprendedores en el Paujil</span>
+                                        </a>
+                                    </div>
+                                    <div class="nav-item next mt_sm--30">
+                                        <a href="#">
+                                            <div class="link-text">
+                                                <p class="">Siguiente</p>
+                                                <span class="fa fa-arrow-right"></span>
+                                            </div>
+                                            <span>Día Mundial de la Leche</span>
+                                        </a>
+                                    </div>
+                                </div>
+                                <!-- End Post Nav Links -->
+
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-12 col-sm-12 col-12 mt_md--60 mt_sm--60">
+                            <div class="blog-sidebar-container">
+                                <!-- Start Sidebar Area -->
+                                <div class="blog-sidebar-wrapper">
+
+                                    <!-- Start Single Widget -->
+                                    <div class="bl-sidebar search">
+                                        <h5 class="widget-title">Buscar</h5>
+                                        <form class="inner" action="#">
+                                            <div class="search-box">
+                                                <input type="text" placeholder="Ingrese palabra clave…">
+                                                <button><span class="fa fa-search"></span></button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <!-- End Single Widget -->
+
+                                   
+
+                                    <!-- Start Single Widget -->
+                                    <div class="bl-sidebar category mt--50 wow move-up">
+                                        <h5 class="widget-title">Categorías</h5>
+                                        <div class="inner">
+                                            <ul class="category-list">
+                                                <li><a href="#">Quesos</a></li>
+                                                <li><a href="#">Lácteos</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <!-- End Single Widget -->
+                                    
+
+                                    <!-- Start Single Widget -->
+                                    <div class="bl-sidebar archive mt--50 wow move-up">
+                                        <h5 class="widget-title">Archives</h5>
+                                        <div class="inner">
+                                            <select>
+                                                <option>Select Month</option>
+                                                <option>January 2019 </option>
+                                                <option>July 2018 </option>
+                                                <option> January 2018 </option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <!-- End Single Widget -->
+
+                                    <!-- Start Single Widget -->
+                                    <div class="bl-sidebar tag mt--50 wow move-up">
+                                        <h5 class="widget-title">Etiquetas</h5>
+                                        <div class="inner">
+                                            <ul class="tagcloud">
+                                                <li><a href="#">ganadería</a></li>
+                                                <li><a href="#">quesos</a></li>
+                                                <li><a href="#">sostenibilidad</a></li>
+                                                
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <!-- End Single Widget -->
+                                </div>
+                                <!-- End Sidebar Area -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Blog Details -->
+
+             <!-- Start Call To Action -->
+             <div class="brook-call-to-action bg_color--43 ptb--70">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-6 col-sm-12 col-12">
+                            <div class="call-content text-center text-md-start text-center">
+                                <h4 class="heading heading-h4 text-black">Quiero mantenerme actualizado de novedades</h4>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-sm-12 col-12">
+                            <div class="text-center text-md-end text-center mt_sm--30 mt_md--40 contact-form contact-form--4 yellow-color-2">
+                                <form action="#">
+                                    <div class="input-box">
+                                        <input type="email" placeholder="Su Correo Electrónico">
+                                        <button>Subscríbete</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Call To Action -->
+        </main>
+        <!--// Page Conttent -->
+
+        <?php
+            include_once 'footer.php';
+        ?>
+
+    </div>
+    <!--// Wrapper -->
+
+    <!-- Js Files -->
+    <!-- <script src="vendor/modernizr-3.6.0.min.js"></script>
+    <script src="js/vendor/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script> -->
+
+    <!-- REVOLUTION JS FILES -->
+    <!-- <script src="js/jquery.themepunch.tools.min.js"></script>
+    <script src="js/jquery.themepunch.revolution.min.js"></script> -->
+
+    <!-- SLIDER REVOLUTION 5.0 EXTENSIONS  (Load Extensions only on Local File Systems !  The following part can be removed on Server for On Demand Loading) -->
+    <!-- <script src="js/revolution.extension.actions.min.js"></script>
+    <script src="js/revolution.extension.carousel.min.js"></script>
+    <script src="js/revolution.extension.kenburn.min.js"></script>
+    <script src="js/revolution.extension.layeranimation.min.js"></script>
+    <script src="js/revolution.extension.migration.min.js"></script>
+    <script src="js/revolution.extension.navigation.min.js"></script>
+    <script src="js/revolution.extension.parallax.min.js"></script>
+    <script src="js/revolution.extension.slideanims.min.js"></script>
+    <script src="js/revolution.extension.video.min.js"></script> -->
+    <!-- Use the minified version files listed below for better performance and remove the files listed above -->
+    <script src="js/vendor/vendor.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <!-- REVOLUTION JS FILES -->
+    <script src="js/revolution.tools.min.js"></script>
+    <!-- SLIDER REVOLUTION 5.0 EXTENSIONS -->
+    <script src="js/revolution.extension.min.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/revoulation.js"></script>
+
+</body>
+
+</html>
